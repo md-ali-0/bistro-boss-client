07002ae6c8a18aeaa4b6c5da1e841853cb1066bf
md-ali-0 <mohammad.98482@gmail.com>
2023-11-22 17:08:32 +0600

    Dashboard Home Added

--- src/Pages/Dashboard/Home/AdminHome.jsx
+++ src/Pages/Dashboard/Home/AdminHome.jsx
@@ -1,7 +1,80 @@
+import { useQuery } from "@tanstack/react-query";
+import { FaBox, FaUsers, FaWallet } from "react-icons/fa";
+import { FaVanShuttle } from "react-icons/fa6";
+import Loading from "../../../Components/Loading/Loading";
+import useAxios from "../../../Hooks/useAxios";
+import Chart from "./BarChart";
+import PieChartComponent from "./PieChart";
+
 const AdminHome = () => {
+    const axios = useAxios();
+    const { data: adminStats=[], isLoading } = useQuery({
+        queryKey: ["adminStats"],
+        queryFn: async () => {
+            const { data } = await axios.get("/admin-stats");
+            return data;
+        },
+    });
+    const { data: dashStats=[] } = useQuery({
+        queryKey: ["dashStats"],
+        queryFn: async () => {
+            const { data } = await axios.get("/order-stats");
+            return data;
+        },
+    });
+    if (isLoading) {
+        return <Loading />;
+    }
+    const { totalUsers, totalOrders, totalMenus, totalRevenue } = adminStats;
     return (
-        <div>
-            <h3>Hi, Welcome Back!</h3>
+        <div className="px-5 py-10">
+            <h3 className="font-Cinzel text-[#151515] text-xl font-bold">Hi, Welcome Back!</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 py-5">
+                <div className="bg-gradient-to-r from-[#BB34F5] to-[#FCDBFF] rounded-xl py-5">
+                    <div className="flex justify-center items-center gap-2 px-3">
+                        <FaWallet size={40} fill="white" />
+                        <dir>
+                            <h3 className="text-2xl font-extrabold text-white">{totalRevenue}</h3>
+                            <p className="text-lg font-medium text-white">Revenue</p>
+                        </dir>
+                    </div>
+                </div>
+                <div className="bg-gradient-to-r from-[#D3A256] to-[#FCDBFF] rounded-xl py-5">
+                    <div className="flex justify-center items-center gap-2 px-3">
+                        <FaUsers size={40} fill="white" />
+                        <dir>
+                            <h3 className="text-2xl font-extrabold text-white">{totalUsers}</h3>
+                            <p className="text-lg font-medium text-white">Customers</p>
+                        </dir>
+                    </div>
+                </div>
+                <div className="bg-gradient-to-r from-[#FE4880] to-[#FCDBFF] rounded-xl py-5">
+                    <div className="flex justify-center items-center gap-2 px-3">
+                        <FaBox size={40} fill="white" />
+                        <dir>
+                            <h3 className="text-2xl font-extrabold text-white">{totalMenus}</h3>
+                            <p className="text-lg font-medium text-white">Products</p>
+                        </dir>
+                    </div>
+                </div>
+                <div className="bg-gradient-to-r from-[#6AAEFF] to-[#FCDBFF] rounded-xl py-5">
+                    <div className="flex justify-center items-center gap-2 px-3">
+                        <FaVanShuttle size={40} fill="white" />
+                        <dir>
+                            <h3 className="text-2xl font-extrabold text-white">{totalOrders}</h3>
+                            <p className="text-lg font-medium text-white">Orders</p>
+                        </dir>
+                    </div>
+                </div>
+            </div>
+            <div className="flex">
+                <div className="md:w-1/2">
+                    <Chart dashStats={dashStats}/>
+                </div>
+                <div className="md:w-1/2">
+                    <PieChartComponent dashStats={dashStats}/>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Dashboard/Home/BarChart.jsx
@@ -0,0 +1,71 @@
+import { Bar, BarChart, CartesianGrid, Cell, XAxis, YAxis } from "recharts";
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+const data = [
+    {
+        name: "Page A",
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: "Page B",
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: "Page C",
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: "Page D",
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+];
+
+const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${
+        y + height
+    }
+    Z`;
+};
+
+const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+const Chart = ({dashStats}) => {
+    return (
+        <BarChart
+            width={500}
+            height={300}
+            data={dashStats}
+            margin={{
+                top: 20,
+                right: 30,
+                left: 20,
+                bottom: 5,
+            }}
+        >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: "top" }}>
+                {dashStats.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                ))}
+            </Bar>
+        </BarChart>
+    );
+};
+
+export default Chart;
